373700936084c6fa3516c1ae7b29f5ae5b0f2189
romanprotoliuk <romanprotoliuk@gmail.com>
2022-04-17 16:12:28 -0700

    slider, buttons work

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -6,8 +6,30 @@ import Box from '@mui/material/Box';
 import * as algorithms from '../../algorithms/Algorithms'
 import { SpeedSlider } from '../SpeedSlider';
 import { PrettoSlider } from '../SpeedSlider2';
+import Backdrop from '@mui/material/Backdrop';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const Body = () => {
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const [arr, setArr] = useState([])
   const [sliderValue, setSlideValue] = useState(3)
@@ -15,6 +37,30 @@ const Body = () => {
   const [valueMatrices, setValueMatrices] = useState({
     entries: 50
   })
+  
+
+
+  // const sizeChange = (event) => {
+  //   console.log(event.target.innerText)
+  //   let num = parseInt(event.target.innerText)
+  //   setSlideValue({ entries:  num})
+  //   changeGraph()
+  //     resetArray()
+  // }
+
+  const sizeChange = () => {
+    setValueMatrices({ entries: 10 })
+    console.log(valueMatrices)
+    changeGraph()
+      resetArray()
+  }
+
+  const sizeChangeTwo = () => {
+    setValueMatrices({ entries: 10 })
+    console.log(valueMatrices)
+    changeGraph()
+      resetArray()
+  }
 
   const valueSetter = (target) => {
     console.log(target)
@@ -27,19 +73,18 @@ const Body = () => {
 
   const changeGraph = () => {
     switch (sliderValue) {
-      case 1:
+      case 10:
         return setValueMatrices({ entries: 10})
-      case 2:
+      case 25:
         return setValueMatrices({ entries: 25})
-      case 3:
+      case 50:
         return setValueMatrices({ entries: 50})
-      case 4:
+      case 100:
         return setValueMatrices({ entries: 100})
-      case 5:
+      case 200:
         return setValueMatrices({entries: 175})
     }
   }
-  console.log(valueMatrices)
 
   useEffect(() => {
     resetArray()
@@ -108,18 +153,17 @@ const Body = () => {
     }
   }
 
-  const handleChange = (event) => {
-    if (event.target.value >= 1 && event.target.value <= 5) {
-      setSlideValue(event.target.value)
+  const handleChange = () => {
       changeGraph()
       resetArray()
-    }
+
   }
 
   useEffect(() => {
     resetArray()
   }, [])
 
+  
 
   const rangeSliderBtn = buttonClicked ? <SpeedSlider aria-label="Temperature" defaultValue={3} marks min={1} max={5} disabled onChange={(value) => valueSetter(value.target.value)}/> : <SpeedSlider aria-label="Temperature" defaultValue={3} marks min={1} max={5} onChange={handleChange} />
 
@@ -150,16 +194,51 @@ const Body = () => {
         <div className='controll-bar'>
           <div className='wrapper-bottom-section'>
             <div className='btn-and-slider-wrapper'>
-              <div className='button-wrapper'>
-                  <button className='btn-generatenew' onClick={resetArray}>Generate array</button>
+
+              <div className='two-button-wrapper'>
+                <div className='button-wrapper'>
+                    <button className='btn-generatenew' onClick={resetArray}>Generate Array</button>
+                  </div>
+                  <div className='button-wrapper'>
+                    <button className='btn-generatenew' onClick={handleOpen}>Custom Array</button>
+                  </div>
               </div>
 
               <div className='slider-wrapper'>
-                <div className='size-wrapper'>
+                {/* <div className='size-wrapper'>
                   <h4 className='h4-size'>size</h4>
                   <Box sx={{ width: 100 }}>
                     {rangeSliderBtn}
                   </Box>
+                </div> */}
+
+                <div className='speed-wrapper'>
+                  <h4 className='h4-speed'>size</h4>
+                  <div className='btn-choice-wrapper'>
+                    <button  onClick={function() { setValueMatrices({entries: 10}, handleChange())}} className='size-btn-choice'>10</button>
+                  </div>
+                  <div className='btn-choice-wrapper'>
+                    <button onClick={sizeChange} className='size-btn-choice'>25</button>
+                  </div>
+                  <div className='btn-choice-wrapper'>
+                    <button onClick={sizeChange} className='size-btn-choice'>50</button>
+                  </div>
+                  <div className='btn-choice-wrapper'>
+                    <button onClick={sizeChange} className='size-btn-choice'>100</button>
+                  </div>
+                  <div className='btn-choice-wrapper'>
+                    <button onClick={sizeChange} className='size-btn-choice'>200</button>
+                  </div>
+                  {/* <Box sx={{ width: 150 }}>
+                    <PrettoSlider
+                      valueLabelDisplay="auto"
+                      aria-label="pretto slider"
+                      defaultValue={50}
+                      min={3}
+                      max={200}
+                      onChange={handleChange}
+                    />
+                  </Box> */}
                 </div>
 
                 <div className='speed-wrapper'>
@@ -183,6 +262,45 @@ const Body = () => {
           </div>
         </div>
       </div>
+
+
+      {/* Modal */}
+      <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Enter Custom Array
+            </Typography>
+            {/* <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography> */}
+            
+            {/* validate numbers */}
+            <TextareaAutosize
+              id='textarea-custom'  
+              aria-label="minimum height"
+              minRows={3}
+              placeholder="1,5,6,2,3"
+              style={{ width: '100%', marginTop: '20px', border: '1px solid #d9d9d9', padding: '4px 11px' }}
+            />  
+            <div className='modal-btn-wrapper'>
+              <button className='btn-custom-arr' onClick={resetArrayAndSetState}>Generate</button>
+            </div>  
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
     </>
   )
 }
